4aee95e757fda42c0fd644a8aeb898dbe303a8a0
Sore <ASORE@graphisoft.com>
2018-04-04 15:07:23 +0200

    listaitem kuldese

--- todo.js
+++ todo.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser');
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: true }));
 
 list = ["Get milk", "Pick up paycheck", "Cash paycheck"]
 
 app.get('/', (req, res) => res.sendFile(path.join (__dirname, 'index.html')))
 
-app.get('/list', (req, res) => {})
+app.get('/list', (req, res) => {
+	res.send(list);
+})
 
-app.put('/item', (req, res) => {})
+app.post('/item', (req, res) => {
+	list.push (req.body.text);
+	res.send();
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
