cbc8b121b93aa21ed96192714830b57960bf8218
Sore <ASORE@graphisoft.com>
2018-04-05 14:13:15 +0200

    csak akkor adjon hozza itemet ha van szoveg

--- ui.js
+++ ui.js
@@ -21,12 +21,23 @@ function clearInput() {
 	todoData.newItem = ''
 }
 
-function getInputElem() {
+function createInputElem() {
 	return m('input', {
 		type: 'text',
 		oninput: m.withAttr("value", (item) => { todoData.newItem = item }),
 		value: todoData.newItem
 	})
+}
+
+function createButton () {
+	return m('button', {
+		onclick: () => {
+			if (todoData.newItem.length > 0) {
+				sendItem();
+				clearInput();
+			}
+		}
+	}, 'Hozzáad')
 }
 
 var Page = {
@@ -35,8 +46,8 @@ var Page = {
 		return m('main', [
 			m('h1', 'Tennivalók'),
 			m('div', [
-				m('form', getInputElem()),
-				m('button', { onclick: () => { sendItem(); clearInput() } }, 'Hozzáad'),
+				m('form', createInputElem()),
+				createButton (),
 				m('ul', todoData.list.map((item) => m('li', item)))
 			])
 		])
