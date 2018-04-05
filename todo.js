a28df2ac6c388ea872b2d59b1030b43cc60b07c9
Sore <ASORE@graphisoft.com>
2018-04-05 11:36:50 +0200

    kliensoldali js kulon szkriptben legyen

--- todo.js
+++ todo.js
@@ -8,6 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 list = ["Get milk", "Pick up paycheck", "Cash paycheck"]
 
 app.get('/', (req, res) => res.sendFile(path.join (__dirname, 'index.html')))
+app.get('/ui.js', (req, res) => res.sendFile(path.join (__dirname, 'ui.js')))
 
 app.get('/list', (req, res) => {
 	res.send(list);
