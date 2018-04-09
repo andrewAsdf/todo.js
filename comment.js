58eb0387d97182200eda63204faf2313befa8fdc
Sore <ASORE@graphisoft.com>
2018-04-09 12:00:49 +0200

    meg tobb eslint jav

--- comment.js
+++ comment.js
@@ -1,13 +1,12 @@
 'use strict';
 
 const Transform = require('stream').Transform;
-const util = require('util');
 
 module.exports = function (text) {
-	return new Transform ({
+	return new Transform({
 		objectMode: true,
 		transform: function (chunk, encoding, callback) {
-			chunk.contents = Buffer.concat ([new Buffer(`/* ${text} */\n`), chunk.contents])
+			chunk.contents = Buffer.concat([new Buffer(`/* ${ text } */\n`), chunk.contents]);
 			callback(null, chunk);
 		}
 	});
